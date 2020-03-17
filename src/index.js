4ff0c58c69ccac9e6592e907f2a70ce327aa8dad
Alexey_Yudin <aviordream@gmail.com>
2020-03-17 23:58:45 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let input = (n < 0) ? -n + '' : n + '';
+    let out = '';
+    for (let i = 0; i < input.length; i++) {
+        out += input[input.length - 1 - i];
+    }
+    return +out
 }
